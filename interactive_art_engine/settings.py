6745180866628e6ad038d1d679e4df672824f78d
Terence Reilly <terryreillyo@gmail.com>
2026-02-17 23:51:28 +0000

    Remove .DS_Store file, update Django settings for template and static file directories, add CSS styles for fonts and layout, and create home.html template with navigation and iframe integration.

--- interactive_art_engine/settings.py
+++ interactive_art_engine/settings.py
@@ -70,10 +70,11 @@ ROOT_URLCONF = 'interactive_art_engine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -130,4 +131,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
