8fb4bb6c6673c8efde26c67fe2481703fa0ae014
Terence Reilly <terryreillyo@gmail.com>
2026-02-17 12:04:29 +0000

    Add ArtworkTemplate and ArtworkInstance models with admin registration; include initial migration

--- artworks/admin.py
+++ artworks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ArtworkTemplate, ArtworkInstance
 
-# Register your models here.
+@admin.register(ArtworkTemplate)
+class ArtworkTemplateAdmin(admin.ModelAdmin):
+	list_display = ('title', 'version', 'created_at')
+
+@admin.register(ArtworkInstance)
+class ArtworkInstanceAdmin(admin.ModelAdmin):
+	list_display = (
+		'firestore_collection_id',
+		'user',
+		'version',
+		'is_active',
+		'start_date',
+		'duration_days'
+	)
+	search_fields = ('firestore_collection_id', 'user__username')
