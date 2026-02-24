330afabb708dc9d7832e234d41e91311fccf58e8
Terence Reilly <terryreillyo@gmail.com>
2026-02-24 23:02:56 +0000

    Fix about view URL in urlpatterns; ensure correct routing for about page

--- interactive_art_engine/urls.py
+++ interactive_art_engine/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -25,6 +24,7 @@ urlpatterns = [
     # Only /dashboard/ (not /artworks/dashboard/) will show the dashboard
     path('dashboard/', include('artworks.urls')),
     path('admin/', admin.site.urls),
+    path('about/', views.about_view, name='about')
 ]
 urlpatterns += [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
